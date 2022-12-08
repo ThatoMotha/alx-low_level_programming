6e8f8229200ba51e3d6bdf9e10cd76ebf86b9f71
ThatoMotha <tmafukati@gmail.com>
2022-12-08 07:58:11 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0
+ * Return: o
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
 
