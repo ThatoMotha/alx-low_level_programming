b8116b5ffa6d8e5013a61e288e5a02f12fa8b483
ThatoMotha <tmafukati@gmail.com>
2022-12-10 22:38:13 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
         printf("Size of long int:%lu byte(s)\n"; sizeof(long int));
         printf("Size of long long int:%lu byte(s)\n"; sizeof(long long int));
 	printf("Size of float:%lu byte(s)\n"; sizeof(float));
-	return(0);
+	return (0);
 
 }
