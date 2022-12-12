b455770442fbd7d3005ca38700d1c1883216f060
ThatoMotha <tmafukati@gmail.com>
2022-12-12 06:23:01 -0800

    5-print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,14 +11,15 @@ int main(void)
 
 	  int i = 0;
 
-	    while (i < 10) {
+	    while (i < 10) 
+	    {
 
-		        printf("%d\n", i);
+		        printf("%d", i);
 
 			    i++;
 
 			      }
-
+		printf("\n");
 	      return 0;
 
 }
