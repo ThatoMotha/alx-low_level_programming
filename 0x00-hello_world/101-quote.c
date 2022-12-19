40589750c837e10f2063fb8571f2ca78d65acb18
ThatoMotha <tmafukati@gmail.com>
2022-12-19 08:55:47 -0800

    redo

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Entry point
+ *Return:1
+*/
+int main(void)
+{
+	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+			return 1;
+		
+}
