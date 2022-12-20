d584be04db499bfd3990081b27f02d0c85399ad2
ThatoMotha <tmafukati@gmail.com>
2022-12-20 11:12:12 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,30 +6,29 @@
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i = 0;
+	int n = 0;
+	int signo = 1;
 
-	while (s[c])
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
-			break;
-		}
-		c++;
+		if (s[i] == '-')
+			signo *= -1;
+		i++;
 	}
-	ni *= min;
-	return (ni);
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	signo *= -1;
+	return (n * signo);
 }
