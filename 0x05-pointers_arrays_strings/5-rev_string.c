35032ba39b15dc4c6b1de2d8ed2f9a7b2b99c06d
ThatoMotha <tmafukati@gmail.com>
2022-12-20 09:29:00 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reversing a string
+ * @s:string to reverse
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int aux = 0;
+	char ltemp;
+
+	while (*(s = i) != '\0')
+		i += 1;
+	i -= 1;
+
+	while (aux < i)
+	{
+		ltemp = s[i];
+		s[i] = s[aux];
+		s[aux] = ltemp;
+		aux++;
+		i--;
+	}
+}
