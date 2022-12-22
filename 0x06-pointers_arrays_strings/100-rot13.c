df7e68de2e3cd807369cac42fff9f2813582b84b
ThatoMotha <tmafukati@gmail.com>
2022-12-22 13:49:05 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,43 @@
 #include "main.h"
 /**
  * rot13 - funtion that encodes a string
- * @str - string being encoded
- * Return: string
+ * @str: string being encoded
+ * Return: encoded string
  */
 char *rot13(char *str)
 {
-	int i = 0;
+	int index1, index2;
 
-	while (str[i] != '\0')
-	{
-		if ((str[i] >= 'a' && str[i] <= 'm') ||
-		(str[i] >= 'A' && str[i] <= 'M'))
-		{
-			 str[i] += 13;
-		}
-		else if ((str[i] >= 'n' && str[i] <= 'z') ||
-			(str[i] >= 'N' && str[i] <= 'Z'))
-		{
-			str[i] -= 13;
-		}
-		i++;
-	}
-	return (str);
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+
+while (str[++index1])
+{
+for (index2 = 0; index2 < 52; index2++)
+{
+if (str[index1] == alphabet[index2])
+{
+str[index1] = rot13key[index2];
+break;
+}
+}
+}
+return (str);
 }
