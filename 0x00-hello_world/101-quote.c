74c4758bf3f0da2c939d59a4af198867b315bfea
ThatoMotha <tmafukati@gmail.com>
2023-01-08 07:46:45 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
- *main - Entry point
- *Return:1
-*/
+ * main - entry point
+ * Return: 1
+ */
 int main(void)
 {
-	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			return 1;
-		
+	char *msg = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(msg, sizeof(char), 50, stderr);
+	return (1);
 }
