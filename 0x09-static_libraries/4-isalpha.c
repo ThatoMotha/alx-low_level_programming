33bec75d586b97bae348821b9d1fac45483e8ed7
ThatoMotha <tmafukati@gmail.com>
2023-01-19 07:54:49 -0800

    task 0

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for char alphabet
+ * @c: is the char to be checked
+ *
+ * Return: 1 if the char is lower or upper, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
