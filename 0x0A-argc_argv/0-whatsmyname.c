02fc9abb067b0ad5fc5739c3adb7b97d905c493a
ThatoMotha <tmafukati@gmail.com>
2023-01-19 02:59:06 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * main - main block
- * Write a program that prints its name, followed by a new line.
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
+ * main - write a program that prints its name, followed by a new line
+ * if you rename the program, it will print the name, without having to
+ * compile it again
+ * you should not remove the path before the name of the program
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ * Return: this return to 0
  */
 int main(int argc, char *argv[])
 {
-	char *program_name = argv[0];
-
-	printf("%s\n", program_name);
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,3 @@
 #ifndef main_h                                                                                                              
 #define main_h 
-int main(int argc, char *argv[]);
-
 #endif   
