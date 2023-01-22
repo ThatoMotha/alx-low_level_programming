836d6aec3a9d0ac52b7274b2edbf757767c5043b
ThatoMotha <tmafukati@gmail.com>
2023-01-22 10:32:02 -0800

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 int sumthem_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /*variadic_functions_h*/
+#endif /*VARIADIC_FUNCTIONS_H*/
